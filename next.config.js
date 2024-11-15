df5bf30ab591b805ebca4e097499a3ff5a3dc95f
aveek-goyal <aveek.goyal@metaschool.so>
2024-11-15 16:39:05 +0530

    Added LICENSE

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true, // Disables ESLint during builds
+    },
     reactStrictMode: true,
     images: {
         domains: ['metaschool.so'],
